055878893f3e9ca382d58a6e92364923583863ec
Anthon Axelsson <anthon.axelsson@learnet.se>
2024-10-08 10:36:22 +0200

    Introt på mitt gladiator spel.

---
+++ gladiaterna.py
@@ -0,0 +1,9 @@
+#Skriva intro text
+#Göra så att spelaren kan anfalla
+#Göra så att motståndare kan anfalla
+#Presentera vem som vann
+
+import random
+
+#intro
+print("Mortis Ludi\n""===========\n""Du är gladiatorn Lady Eater, nu ska du slåss mot gladiatorn Guts.\n""Ni befinner er i en stor romersk arena i Nicaragua omgivna av en förväntansful \npublik."" Ni har inga vapen eller rustning utan du är klädd enbart i ett par\n""baggy jeans, Vlone shirt, ett par jordan 1s och du mot all förmodan \nråkar vara 6,4 meter lång med dreads."" Motståndaren har slim jeans \noch en polo tröja... han vill ha dina kläder.\n""Han ser redo ut att anfalla dig, gör dig redo.")
